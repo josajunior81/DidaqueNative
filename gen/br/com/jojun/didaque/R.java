452814c063bf6b0d1d2b863e9fe5a3556cc70fa5
josafa.souza <josafa.souza@serpro.gov.br>
2013-09-27 16:34:42 -0300

    Quase lá

--- gen/br/com/jojun/didaque/R.java
+++ gen/br/com/jojun/didaque/R.java
@@ -1108,10 +1108,11 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030016;
         public static final int abc_search_view=0x7f030017;
         public static final int activity_dashboard=0x7f030018;
-        public static final int adapter_list_apostila=0x7f030019;
-        public static final int fragment_view_licao=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
-        public static final int viewpager_apostila=0x7f03001c;
+        public static final int activity_splash=0x7f030019;
+        public static final int adapter_list_apostila=0x7f03001a;
+        public static final int fragment_view_licao=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int viewpager_apostila=0x7f03001d;
     }
     public static final class menu {
         public static final int dashboard=0x7f0d0000;
