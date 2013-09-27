81fbc07bdf64a4697622b764233025a0e027f6c6
josafa.souza <josafa.souza@serpro.gov.br>
2013-09-27 09:57:26 -0300

    Primeiro commit

---
+++ src/br/com/jojun/didaque/DidaqueApplication.java
@@ -0,0 +1,28 @@
+package br.com.jojun.didaque;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+
+public class DidaqueApplication  extends Application {
+
+	private static final String TAG = DidaqueApplication.class.getSimpleName();
+
+	private static Context context;
+
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		context = this;
+
+		Log.i(TAG, "Fabricante: " + Build.MANUFACTURER);
+		Log.i(TAG, "Aparelho: " + Build.MODEL);
+		Log.i(TAG, "Versão: " + Build.VERSION.RELEASE);
+	}
+
+	public static Context getContext() {
+		return context;
+	}
+}
---
+++ src/br/com/jojun/didaque/fragment/ApostilasFragment.java
@@ -0,0 +1,7 @@
+package br.com.jojun.didaque.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class ApostilasFragment extends Fragment {
+
+}
